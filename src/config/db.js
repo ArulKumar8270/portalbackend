b962e49f7a0f13bdd64e0435ecbcc6159466176c
arul <arul@aruls-MacBook-Air.local>
2026-07-28 12:51:26 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -3,35 +3,35 @@ module.exports = {
    * Here you may specify which of the database connections below you wish
    * to use as your default connection for all database work.
    */
-  connection: process.env.DB_CONNECTION || "mysql",
+  connection:  "mysql",
 
   /**
    * Here you may specify the host address of database which will be
    * used for connection
    */
-  host: process.env.DB_HOST || "82.25.121.111",
+  host: "82.25.121.111",
 
   /**
    * Here you may specify the port of database which will be
    * used for connection
    */
-  port: process.env.DB_PORT || 3306,
+  port:  3306,
 
   /**
    * Here you may specify the database name of connection which will be
    * used for connection
    */
-  database: process.env.DB_DATABASE || "u490757224_nicknameportal",
+  database: "u490757224_nicknameportal",
 
   /**
    * Here you may specify the username of database which will be
    * used for connection
    */
-  username: process.env.DB_USERNAME || "u490757224_nicknameportal",
+  username: "u490757224_nicknameportal",
 
   /**
    * Here you may specify the password of database which will be
    * used for connection
    */
-  password: process.env.DB_PASSWORD || "mQ9rjpQxyL~*",
+  password: "mQ9rjpQxyL~*",
 };
